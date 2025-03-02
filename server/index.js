945d71f252d07d5e32bbbb9f3d430a4c0620102c
Van5sh <vansh05dhir@gmail.com>
2025-03-02 18:28:54 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("client ready", (data) => {
+        console.log("Client says:", data);
+    });
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("message", data);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
